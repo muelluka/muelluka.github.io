e0313b426e7384965ded885994074e6c8928282a
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-26 16:05:51 +0100

    strings in javascript

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,22 @@
 // alert("Hello world")
+
+// let message = "Hello world"
+// alert(message);
+// message = "Hallo welt"
+// alert(message);
+
+const LINK_COLOR = "#ff0000"
+console.log("Link bitte in der Farbe ", LINK_COLOR)
+
+let highscore = 520233;
+console.log(highscore / 10);
+
+let firstname = "Harry"
+let lastname = "Hirsch"
+
+console.log("Name: ", firstname, lastname)
+
+let fullname = 'Jeffrey "The Dude" Lebowski'
+console.log(fullname)
+let template = `Dein Highscore ist ${highscore} Punkte`;
+console.log(template)
