f2ca69843a492b7eb39c7b1214223a7ecbfa19be
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-26 16:26:55 +0100

    arrays/objects

--- javascript/hello.js
+++ javascript/hello.js
@@ -5,18 +5,53 @@
 // message = "Hallo welt"
 // alert(message);
 
-const LINK_COLOR = "#ff0000"
-console.log("Link bitte in der Farbe ", LINK_COLOR)
+const LINK_COLOR = "#ff0000";
+console.log("Link bitte in der Farbe ", LINK_COLOR);
 
 let highscore = 520233;
 console.log(highscore / 10);
 
-let firstname = "Harry"
-let lastname = "Hirsch"
+let firstname = "Harry";
+let lastname = "Hirsch";
 
-console.log("Name: ", firstname, lastname)
+console.log("Name: ", firstname, lastname);
 
-let fullname = 'Jeffrey "The Dude" Lebowski'
-console.log(fullname)
+let fullname = 'Jeffrey "The Dude" Lebowski';
+console.log(fullname);
+
 let template = `Dein Highscore ist ${highscore} Punkte`;
-console.log(template)
+console.log(template);
+
+let isOver18 = true;
+console.log(isOver18);
+
+let age = 17;
+console.log("über 18?", age > 18);
+
+let participants = ["Detlef", "Jan", "Hiasi"];
+console.log(participants);
+console.log("Einträge im Array:", participants.length);
+console.log(participants[2]);
+
+gameHighscore = [2099, 3010, 3330, 5000];
+console.log(gameHighscore);
+
+let user = {
+    firstname: "Sepp",
+    lastname: "Fent",
+    age: 25
+};
+console.log(user);
+console.log(user.firstname);
+user.highscore = 200;
+console.log(user);
+user["hihscore ever"] = 400;
+console.log(user);
+
+let a = 2;
+let b = 4;
+console.log(a+b);
+console.log(b/(a-1));
+a++;
+console.log(a);
+
