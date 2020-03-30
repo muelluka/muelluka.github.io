71f71ed06892f02a6bc3d981f00c728fb63c3da9
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-30 16:55:56 +0200

    Fehler in index.html behoben und Popup hinzugefügt

--- nz/main.js
+++ nz/main.js
@@ -2,6 +2,7 @@ let map = document.querySelector("#map")
 
 let lat = map.dataset.lat;
 let lng = map.dataset.lng;
+let title = map.dataset.title
 
 var mymap = L.map('map').setView([lat, lng], 13);
 
@@ -9,3 +10,8 @@ L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
+
+
+var popup = L.marker([lat, lng]).addTo(mymap);
+
+popup.bindPopup(title).openPopup();
