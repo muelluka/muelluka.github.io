a78af662c6baa104fdc02aaafc6b9c831a6ecd1b
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-03-30 11:44:44 +0200

    Versuch die Karte zu implementieren

--- nz/main.js
+++ nz/main.js
@@ -1,1 +1,11 @@
-//alert("Hallo Welt")
+let map = document.querySelector("#map")
+
+let lat = map.dataset.lat;
+let lng = map.dataset.lng;
+
+var mymap = L.map('map').setView([lat, lng], 13);
+
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
