bd9048ca34873991655f37b53ad583d075f01fde
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-04-02 18:09:03 +0200

    Recovered Datensatz mit funktion aufgerufen

--- world/main.js
+++ world/main.js
@@ -8,36 +8,45 @@ let map = L.map("map", {
     ]
 });
 
+let circleGroup = L.featureGroup().addTo(map);
+
 L.control.layers({
     "OpenTopoMap": startLayer,
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
     "OpenStreetMap.DE": L.tileLayer.provider("OpenStreetMap.DE"),
     "OpenStreetMap.HOT": L.tileLayer.provider("OpenStreetMap.HOT"),
     "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads"),
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map)
 
-L.marker([0, 0]).addTo(map);
+let drawCircles = function (data) {
+    for (let i = 1; i < data.length; i++) {
+        let row = data[i];
+        //console.log(row[2],row[3]);
+        let lat = row[2]
+        let lng = row[3]
+        let val = row[row.length - 1];
+        // let mrk = L.marker([lat, lng]).addTo(map);
+        // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
 
+        let s = 0.25
+
+        // A = r^2 *PI
+        // r^2 = A/PI
+        // r = Wurzel(A/PI)
+        let r = Math.sqrt(val * s / Math.PI);
+
+        let circle = L.circleMarker([lat, lng], {
+            radius: r
+        }).addTo(circleGroup);
+
+        circle.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+    }
+};
+
+// drawCircles(CONFIRMED);
+drawCircles(RECOVERED);
+
+// L.marker([0, 0]).addTo(map);
 //console.log(CONFIRMED);
-for (let i = 1; i < CONFIRMED.length; i++) {
-    let row = CONFIRMED[i];
-    //console.log(row[2],row[3]);
-    let lat = row[2]
-    let lng = row[3]
-    let val = row[row.length - 1];
-    // let mrk = L.marker([lat, lng]).addTo(map);
-    // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
-
-    let s = 0.25
-
-    // A = r^2 *PI
-    // r^2 = A/PI
-    // r = Wurzel(A/PI)
-    let r = Math.sqrt(val*s/Math.PI);
-
-    let circle = L.circleMarker([lat, lng],{
-        radius: r
-    }).addTo(map);
-
-    circle.bindPopup(`${row[0]} ${row[1]}: ${val}`);
-}
