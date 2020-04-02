bbe5e9a2d72c325eb405f2400673d99f13258c5e
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-04-02 17:47:55 +0200

    Größenkreise nach Fläche mit Popup

--- world/main.js
+++ world/main.js
@@ -16,13 +16,28 @@ L.control.layers({
     "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads"),
 }).addTo(map)
 
-L.marker([0,0]).addTo(map);
+L.marker([0, 0]).addTo(map);
 
 //console.log(CONFIRMED);
 for (let i = 1; i < CONFIRMED.length; i++) {
     let row = CONFIRMED[i];
     //console.log(row[2],row[3]);
-    let val = row[row.length-1];
-    let mrk = L.marker([row[2],row[3]]).addTo(map);
-    mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+    let lat = row[2]
+    let lng = row[3]
+    let val = row[row.length - 1];
+    // let mrk = L.marker([lat, lng]).addTo(map);
+    // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+
+    let s = 0.25
+
+    // A = r^2 *PI
+    // r^2 = A/PI
+    // r = Wurzel(A/PI)
+    let r = Math.sqrt(val*s/Math.PI);
+
+    let circle = L.circleMarker([lat, lng],{
+        radius: r
+    }).addTo(map);
+
+    circle.bindPopup(`${row[0]} ${row[1]}: ${val}`);
 }
