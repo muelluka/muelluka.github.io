f6a5ab65c469f05a121dfe206890ea590a9d2e2a
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-04-02 17:02:19 +0200

    Anpassung mit provider

--- nz/main.js
+++ nz/main.js
@@ -1,30 +1,26 @@
-let map = document.querySelector("#map")
+let map = document.querySelector("#map");
 
 let lat = map.dataset.lat;
 let lng = map.dataset.lng;
 let title = map.dataset.title;
 
-var mymap = L.map('map').setView([lat, lng], 13);
+// var mymap = L.map('map').setView([lat, lng], 13);
 
 // L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 17,
 //     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 // }).addTo(mymap);
 
-let map = L.map("map", {
+let mymap = L.map("map", {
     center: [
-        mapdiv.dataset.lat,
-        mapdiv.dataset.lng
+        lat,
+        lng
     ],
-    zoom: 17,
+    zoom: 13,
     layers: [
-        L.tileLayer.provider('Stamen.Watercolor')
+        L.tileLayer.provider("OpenTopoMap")
     ]
 });
-
-
-
-
 
 var popup = L.marker([lat, lng]).addTo(mymap);
 
