76aabd43a31d1baafe1dbbfb307cb853d43e779d
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-04-02 18:23:44 +0200

    Beautify

--- world/main.js
+++ world/main.js
@@ -46,8 +46,8 @@ let drawCircles = function (data) {
 };
 
 // drawCircles(CONFIRMED);
-// drawCircles(RECOVERED);
-drawCircles(DEATH);
+drawCircles(RECOVERED);
+// drawCircles(DEATH);
 
 // L.marker([0, 0]).addTo(map);
 //console.log(CONFIRMED);
