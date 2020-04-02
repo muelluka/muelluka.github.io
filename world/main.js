8b486101272a88b7392ac8c023a5f7263c0d51dc
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-04-02 17:03:37 +0200

    Layer hinzugefügt

--- world/main.js
+++ world/main.js
@@ -1,7 +1,18 @@
+let startLayer = L.tileLayer.provider("OpenTopoMap");
+
 let map = L.map("map", {
     center: [0, 0],
     zoom: 2,
     layers: [
-        L.tileLayer.provider("OpenTopoMap")
+        startLayer
     ]
-})
+});
+
+L.control.layers({
+    "OpenTopoMap": startLayer,
+    "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "OpenStreetMap.DE": L.tileLayer.provider("OpenStreetMap.DE"),
+    "OpenStreetMap.HOT": L.tileLayer.provider("OpenStreetMap.HOT"),
+    "OpenMapSurfer.Roads": L.tileLayer.provider("OpenMapSurfer.Roads"),
+
+}).addTo(map)
