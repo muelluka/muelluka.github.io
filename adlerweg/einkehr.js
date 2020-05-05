f198ad0282feb7a055219eb1e9b5137242b3d7ed
Lukas Müller <Lukas.V.Mueller@student.uibk.ac.at>
2020-05-05 18:13:58 +0200

    einkehr.js hinzugefügt und Einkehr für Etappe 17 und 18

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,10 @@
+const EINKEHR = [
+    [Etappennummer(18), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(18), LoreaHuette, 47.34651, 10.79125],
+    [Etappennummer(18), HintereTarrentonalm, 47.32794, 10.72056],
+    [Etappennummer(18), AnhalterHuette, 47.30128, 10.66784],
+    [Etappennummer(17), Grubigalm, 47.38479, 10.86268],
+    [Etappennummer(17), HotelSchlossFernstein, 47.34563, 10.81764],
+    [Etappennummer(17), LoreaHuette, 47.34651, 10.79125],
+
+];
